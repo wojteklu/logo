cb9610e6ebdbda9bc64cea41c6af2813b8aceb1e
Wojtek Lukaszuk <lukaszuk.wojtek@gmail.com>
2018-01-10 17:24:25 +0100

    Update to Swift 4 (#5)

--- Logo/Playground.playground/Pages/Colors.xcplaygroundpage/Contents.swift
+++ Logo/Playground.playground/Pages/Colors.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 import Logo
 
-let code = "setColor(yellow) repeat 5 [fd(300) rt(144)]"
+let code = "setColor(blue) repeat 5 [fd(300) rt(144)]"
 let interpreter = Interpreter()
 interpreter.run(code: code)
